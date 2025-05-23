80c1a7f97fa9997a708032099f911429009b6ab1
jgabrielplopes <jgabrielplopes@gmail.com>
2025-05-23 13:05:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ DB_CONFIG = {
     "database": "votacao_yh36",
     "user": "votacao_yh36_user",
     "password": "wTvdT9dkt02d3o6AriJb27V9Aen6DAVs",
-    "port": "5000"
+    "port": "5432"
 }
 
 def get_db():
@@ -359,4 +359,5 @@ def criar_votacao():
     """, erro=erro)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

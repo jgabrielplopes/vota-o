a7fb84a9e3eaff07c1b72a38e159fbf651d14bad
jgabrielplopes <jgabrielplopes@gmail.com>
2025-05-23 12:43:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -359,5 +359,4 @@ def criar_votacao():
     """, erro=erro)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5432))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
